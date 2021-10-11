33696d3991db91fb8e950e1e2e492e3e5d2a1c41
Babak Shirvani <babak@MacBook-Pro.local>
2021-10-11 16:04:23 -0700

    now answer validation function will return true or false

--- questions.rb
+++ questions.rb
@@ -1,31 +1,31 @@
 class Question
-
+  attr_reader :question, :answer
   def initialize(player)
     @player = player
   end
 
   def ask_question
     make_question
-    puts question
+    puts @question
     puts "> "
   end
 
   def answer_validation (answer)
-    if answer == @answer
-      puts "yesssss!"
+    if answer === @answer
+      puts "Yesssss! You are correct."
+      true
     else
       puts "whatttttt???? No!!"
+      false
     end
   end
 
 
   def make_question
-    number1 = round(1..20)
-    number2 = round(1..20)
-    @question = "#{@player}: What does #{number1} plus #{number2} equal?"
-    @answer = number1 + number2
+    number_1 = rand(1..20)
+    number_2 = rand(1..20)
+    @question = "#{@player}: What does #{number_1} plus #{number_2} equal?"
+    @answer = number_1 + number_2
   end
-
-
 
 end
